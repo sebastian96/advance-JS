9996c0d67db70b11a7c5626a7dce14147ff292e5
sebastian <arqcompu4@gmail.com>
2019-10-06 13:22:25 -0500

    cambios

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+
+function MediaPlayer(config){
+    this.media = config.el;
+}
+
+MediaPlayer.prototype.play = function() {
+    if(this.media.paused) {
+        this.media.play();
+    } else {
+        this.pause();
+    }
+}
+
+MediaPlayer.prototype.pause = function() {
+    this.media.pause();
+}
+
+const player = new MediaPlayer( {el: video} );
+
+button.onclick = () => player.play();
