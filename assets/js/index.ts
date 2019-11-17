c78cbfb1cebb151646c836cfe216a58d5aaeab58
Sebastian Miranda <arqcompu4@gmail.com>
2019-11-16 22:47:29 -0500

    ads agregados

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const button: HTMLElement = document.querySelector('#play');
@@ -10,7 +11,8 @@ const player = new MediaPlayer({
     el: video,
     plugins: [
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new Ads()
     ] 
 });
 
